4f5befa38925a5d803b1bd752d09daf23e357bf5
Idan Mosseri <mosseridan@gmail.com>
2018-03-03 15:46:12 +0200

    started working on add sensor

--- angular-src/src/app/components/add-sensor/add-sensor.component.ts
+++ angular-src/src/app/components/add-sensor/add-sensor.component.ts
@@ -6,6 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-sensor.component.css']
 })
 export class AddSensorComponent implements OnInit {
+  types = [
+    "Automotive",
+    "Digital Component",
+    "Flow",
+    "Fluid Property",
+    "Force",
+    "Humidity",
+    "Mass Air Flow",
+    "Multi-Sensor Module",
+    "Photo Optic",
+    "Piezo Film",
+    "Position",
+    "Pressure",
+    "Rate & Inertial",
+    "Scanner & System",
+    "Speed",
+    "Temperature",
+    "Torque",
+    "Traffic",
+    "Ultrasonic",
+    "Vibration",
+    "Water-Level",
+    "Other"
+  ];
+
+  manufacturers = [
+    "TE Connectivity",
+    "Sonnoff",
+    "Other"
+  ];
+
+  gateways = [
+    "gateway1",
+    "gateway2",
+    "gateway3",
+    "gateway4",
+    "gateway5"
+  ];
+
+  displayName = "";
+
 
   constructor() { }
 
